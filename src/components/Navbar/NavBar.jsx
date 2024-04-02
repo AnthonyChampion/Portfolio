54dc15b1fd9dbe6f4db2d6886955f6dff234cc6c
Anthony Champion Rodrigues <159131513+AnthonyChampion@users.noreply.github.com>
2024-04-02 12:51:29 +0200

    Update NavBar.jsx

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -24,16 +24,16 @@ export const Navbar = () => {
                 onClick={() => setMenuOpen(false)}>
 
                 < li >
-                    <a href="about">About</a>
+                    <a href=""about">About</a>
                 </li>
                 <li>
-                    <a href="about">Experience</a>
+                    <a href="#experience">Experience</a>
                 </li>
                 <li>
-                    <a href="about">Projects</a>
+                    <a href="#projects">Projects</a>
                 </li>
                 <li>
-                    <a href="about">Contact</a>
+                    <a href="#contact">Contact</a>
                 </li>
             </ul>
         </div >
